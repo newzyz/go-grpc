cf9c110de403766a22fba5c20f348c89a37c4d02
newzyz <nooknet102@gmail.com>
2022-08-15 15:58:12 +0700

    httpUpload

--- server/server.go
+++ server/server.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
 
+	"github.com/google/uuid"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
 	_ "github.com/lib/pq"
@@ -29,6 +34,7 @@ func main() {
 	go func() {
 		//mux
 		mux := runtime.NewServeMux()
+		mux.HandlePath("POST", "/uploadFileHttp", handleBinaryFileUpload)
 
 		//Run concurrent
 		pb.RegisterBookHandlerServer(context.Background(), mux, bookSrv)
@@ -52,4 +58,55 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to server %v", err)
 	}
+}
+func handleBinaryFileUpload(w http.ResponseWriter, r *http.Request, params map[string]string) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to parse form: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	f, header, err := r.FormFile("attachment")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to get file 'attachment': %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+	defer f.Close()
+	// open output file
+	fmt.Println(header.Filename)
+	fileExtension := filepath.Ext(header.Filename)
+	filename := uuid.New().String() + fileExtension
+	fo, err := os.Create("./server/up/" + filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// make a buffer to keep chunks that are read
+	buf := make([]byte, 1024)
+	for {
+		// read a chunk
+		n, err := f.Read(buf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		if n == 0 {
+			break
+		}
+
+		// write a chunk
+		if _, err := fo.Write(buf[:n]); err != nil {
+			panic(err)
+		}
+	}
+	//
+	// Now do something with the io.Reader in `f`, i.e. read it into a buffer or stream it to a gRPC client side stream.
+	// Also `header` will contain the filename, size etc of the original file.
+	//
 }
